cdaf9e4a467a5304737a54ae83536e26f88283b6
Saharsh Bawankar(SDB) <bawankar.saharsh@gmail.com>
2021-04-19 18:58:22 -0400

    nhtsa work.

---
+++ Readme.md
@@ -0,0 +1,2 @@
+by default the search bar page will be shown.
+add /api-docs for getting documentation
---
+++ public/search.js
@@ -0,0 +1,60 @@
+let rawOutput = [];
+const loadCharacters = async() => {
+    const searchVin = document.getElementById("searchBar");
+    let vin = searchVin.value;
+    var outcome = await fetch(
+        "https://vpic.nhtsa.dot.gov/api//vehicles/DecodeVin/" + vin + "?format=json"
+    ).then((r) => r.json());
+
+    let output = {};
+    // let noNull = [];
+    for (i = 0; i < outcome.Results.length; i++) {
+        output[outcome.Results[i].Variable] = outcome.Results[i].Value;
+    }
+    output = onlyValidOutput(output);
+    show(output, vin)
+};
+
+const onlyValidOutput = (obj) => {
+    for (var propName in obj) {
+        if (obj[propName] === null || obj[propName] === "Not Applicable" || obj[propName] === "") {
+            delete obj[propName];
+        }
+        delete obj['Error Code'];
+        delete obj['Error Text'];
+    }
+    return obj
+};
+
+function show(data, vin) {
+    let tab =
+        `<thead>
+        <tr>
+        <th>Variables</th>
+        <th>Values</th>
+       </tr>
+       </thead>`;
+
+    // Loop to access all rows 
+    tab += `<tbody>
+            <tr> 
+            <td>VIN</td>
+            <td>${vin}</td>
+            </tr>`
+    for (let r of Object.keys(data)) {
+        tab += `<tr> 
+        <td>${r} </td>
+        <td>${data[r]}</td>
+        </tr>`;
+    }
+    tab += `</tbody>`
+
+    // Setting innerHTML as tab variable
+    document.getElementById("table1").innerHTML = tab;
+}
+
+document.getElementById("searchBtn").addEventListener("click", function() {
+    loadCharacters();
+    const searchVin = document.getElementById("searchBar");
+    document.getElementById("hi").innerHTML = searchVin.value;
+});
